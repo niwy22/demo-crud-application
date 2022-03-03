a487a94c3676bd8511e1b38bcfbb1946109a1998
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-03 16:43:13 +0530

    Update get-version.js

--- src/get-version.js
+++ src/get-version.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
-const doc = yaml.load(fs.readFileSync("./main/swagger.yaml", "utf8"));
+const doc = yaml.load(fs.readFileSync("./main/demo-crud-project.json", "utf8"));
 console.log(doc.info.version)
