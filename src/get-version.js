2ad6bb45444efeb9c9a4eb5224efc715ef0216e1
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-03 17:31:15 +0530

    Delete get-version.js

--- src/get-version.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const yaml = require("js-yaml");
-const doc = yaml.load(fs.readFileSync("./https://github.com/niwy22/demo-crud-application.git/src/main/demo-crud-project.json", "utf8"));
-console.log(doc.info.version)
