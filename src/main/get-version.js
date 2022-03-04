455863d9856abb5172f390753a361b931a084b35
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-04 16:42:14 +0530

    Update get-version.js

--- src/main/get-version.js
+++ src/main/get-version.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
-const doc = yaml.load(fs.readFileSync("././pom.xml", "utf8"));
-console.log(root.project.version)
+//const doc = yaml.load(fs.readFileSync("././pom.xml", "utf8"));
+const doc = ext.parsePom({ filePath: ././pom.xml"});
+var version= doc.version;
+console.log(version)
+//console.log(root.project.version)
