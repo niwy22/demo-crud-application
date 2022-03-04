d5f03ae8256c7fb968f1cc3a0b9f31b2fe1ca09d
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-04 16:59:19 +0530

    Update get-version.js

--- src/main/get-version.js
+++ src/main/get-version.js
@@ -1,4 +1,4 @@
 var ext = require('pom-parser'); 
-var pom = pomParser.parse({ filePath: "././pom.xml"});
+var pom = ext.parse({ filePath: "././pom.xml"});
 var version = pom.version;
 console.log(version)
