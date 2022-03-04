e72040c5fc298018682b9ef35ddc65a0a7a3606c
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-04 17:53:18 +0530

    Update get-version.js

--- src/main/get-version.js
+++ src/main/get-version.js
@@ -1,4 +1,4 @@
 var ext = require("pom-parser");
-var pom = ext.parse({ filePath: "././pom.xml"});
+var pom = ext.parse({ filePath: "././https://github.com/niwy22/demo-crud-application.git/pom.xml"});
 console.log(pom);
 
