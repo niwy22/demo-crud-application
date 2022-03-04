ce7942747ffa22f608f50f3e76bfcde0f403461f
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-04 17:32:42 +0530

    Update get-version.js

--- src/main/get-version.js
+++ src/main/get-version.js
@@ -1,8 +1,16 @@
-var ext = require('pom-parser'); 
-var pom = ext.parse({ filePath: "././pom.xml"});
-var version = pom.version;
-console.log(version)
-module.exports = function(grunt)
- {
-//GRUNT ....
-}
+var pomParser = require("pom-parser");
+var opts = {
+  filePath: "././pom.xml", 
+};
+
+pomParser.parse(opts, function(err, pomResponse) {
+  if (err) {
+    console.log("ERROR: " + err);
+    process.exit(1);
+  }
+
+ 
+  console.log("XML: " + pomResponse.pomXml);
+  // The parsed pom pbject.
+  console.log("OBJECT: " + JSON.stringify(pomResponse.pomObject));
+});
