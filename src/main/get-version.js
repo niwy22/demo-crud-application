f822abff5e37273376be1fbebcd4c86fb524a075
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-04 18:42:31 +0530

    Update get-version.js

--- src/main/get-version.js
+++ src/main/get-version.js
@@ -1,4 +1,16 @@
-var ext = require("pom-parser");
-var pom = ext.parse({ filePath: "././https://github.com/niwy22/demo-crud-application.git/pom.xml"});
-console.log(pom);
+var pomParser = require("pom-parser");
+var opts = {
+  filePath:  __dirname + "././pom.xml", // The path to a pom file
+};
+pomParser.parse(opts, function(err, pomResponse) {
+  if (err) {
+    console.log("ERROR: " + err);
+    process.exit(1);
+  }
 
+  // The original pom xml that was loaded is provided.
+  console.log("XML: " + pomResponse.pomXml);
+  // The parsed pom pbject.
+  console.log("OBJECT: " + JSON.stringify(pomResponse.pomObject));
+  console.log(project.version);
+});
