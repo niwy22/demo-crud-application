307b1396c108b28a4088f45ce98fb988615b9640
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-07 13:23:50 +0530

    Create get-version.js

---
+++ bin/get-version.js
@@ -0,0 +1,29 @@
+var pomParser = require("pom-parser");
+const fs = require("fs");
+const yaml = require("js-yaml");
+var opts = {
+  filePath: "././pom.xml", // The path to a pom file
+};
+// Parse the pom based on a path
+pomParser.parse(opts, function(err, pomResponse) {
+  if (err) {
+    console.log("ERROR: " + err);
+    process.exit(1);
+  }
+  var xml=pomResponse.pomXml;
+  //console.log(xml.project)
+  //console.log("XML: " + pomResponse.pomXml);
+  // The parsed pom pbject.
+  var json=JSON.stringify(pomResponse.pomObject);
+  var store=json
+  console.log(store)
+  //console.log(store.project.version)
+  fs.writeFile('./src/main/test.yaml', store, err => {
+  if (err) {
+    console.error(err)
+    return store
+  }
+  //file written successfully
+})
+
+});
