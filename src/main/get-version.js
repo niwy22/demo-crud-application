620b0a07c2d52d9ac0299e1ace47833970ba7a87
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-07 12:16:00 +0530

    Update get-version.js

--- src/main/get-version.js
+++ src/main/get-version.js
@@ -2,7 +2,7 @@ var pomParser = require("pom-parser");
 const fs = require("fs");
 const yaml = require("js-yaml");
 var opts = {
-  filePath: "C:/Users/niweditar/Videos/demo-crud-application/pom.xml", // The path to a pom file
+  filePath: "././pom.xml", // The path to a pom file
 };
 // Parse the pom based on a path
 pomParser.parse(opts, function(err, pomResponse) {
@@ -16,6 +16,14 @@ pomParser.parse(opts, function(err, pomResponse) {
   // The parsed pom pbject.
   var json=JSON.stringify(pomResponse.pomObject);
   var store=json
-  console.log(store.project)
-  
+  console.log(store)
+  //console.log(store.project.version)
+  fs.writeFile('./test.yaml', store, err => {
+  if (err) {
+    console.error(err)
+    return store
+  }
+  //file written successfully
+})
+
 });
