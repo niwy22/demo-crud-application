5e178f5e7b51a80d2f87e10d2118b6fc71eaf9e6
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-07 11:44:36 +0530

    Update get-version.js

--- src/main/get-version.js
+++ src/main/get-version.js
@@ -1,12 +1,21 @@
-var convert = require('xml-js');
-ignoreUndefined = true;
-var xml = require('fs').readFileSync('././pom.xml', 'utf8');
-
-var result = convert.xml2json(xml, {compact: true, spaces: 4});
-console.log(result);
-var json=JSON.stringify(result);
-//var project= xmlDoc.getElementsByTagName("version")[0];
-//var result1 = convert.xml2json(project, {compact: true, spaces: 4});
-//const errorNode = result.querySelector("parseerror");
-const project= result.project.artifactId._text;
-console.log(project);
+var pomParser = require("pom-parser");
+const fs = require("fs");
+const yaml = require("js-yaml");
+var opts = {
+  filePath: "C:/Users/niweditar/Videos/demo-crud-application/pom.xml", // The path to a pom file
+};
+// Parse the pom based on a path
+pomParser.parse(opts, function(err, pomResponse) {
+  if (err) {
+    console.log("ERROR: " + err);
+    process.exit(1);
+  }
+  var xml=pomResponse.pomXml;
+  //console.log(xml.project)
+  //console.log("XML: " + pomResponse.pomXml);
+  // The parsed pom pbject.
+  var json=JSON.stringify(pomResponse.pomObject);
+  var store=json
+  console.log(store)
+  
+});
