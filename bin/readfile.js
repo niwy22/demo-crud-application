4ca910a128b16012eb034113c88534b3d3dfa3f0
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-07 14:46:55 +0530

    Update readfile.js

--- bin/readfile.js
+++ bin/readfile.js
@@ -1,4 +1,16 @@
-const fs = require("fs");
-const yaml = require("js-yaml");
-const doc = yaml.load(fs.readFileSync("./src/main/test.yaml", "utf8"));
-console.log(doc.project.version)
+const xml2js = require('xml2js');
+const fs = require('fs');
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
+
+// this example reads the file synchronously
+// you can read it asynchronously also
+let xml_string = fs.readFileSync("C:/Users/niweditar/Videos/demo-crud-application/pom.xml", "utf8");
+
+parser.parseString(xml_string, function(error, result) {
+    if(error === null) {
+        console.log(result.project.version);
+    }
+    else {
+        console.log(error);
+    }
+});
