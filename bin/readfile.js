ac31b96c190241dae64fca91ea6d66f37fdc6fc9
niwy22 <97876883+niwy22@users.noreply.github.com>
2022-03-08 11:29:55 +0530

    Update readfile.js

--- bin/readfile.js
+++ bin/readfile.js
@@ -4,18 +4,18 @@ const parser = new xml2js.Parser({ attrkey: "ATTR" });
 
 // this example reads the file synchronously
 // you can read it asynchronously also
-let xml_string = fs.readFileSync("./pom.xml", "utf8");
-var con1
+let xml_string = fs.readFileSync("C:/Users/niweditar/Videos/demo-crud-application/pom.xml", "utf8");
+var version;
+var name;
 parser.parseString(xml_string, function(error, result) {
     if(error === null) {
-		//console.log(result);
-        con1=result.project.version;
+		name=result.project.name[0];
+        version=result.project.version[0];
     }
     else {
         console.log(error);
     }
 });
 
-var json=JSON.stringify(con1)
-console.log(json.replace(/["\[\]']+/g,''));
-
+var version=console.log(version);
+var name=console.log(name);
